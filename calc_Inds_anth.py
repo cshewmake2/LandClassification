7a9ee3f87814af6d9fcdfaf64a5f61e6e29b8390
golnoosh9 <g.dehghanpoor@wustl.edu>
2017-10-03 16:04:06 -0500

    indices_check

--- calc_Inds_anth.py
+++ calc_Inds_anth.py
@@ -3,38 +3,35 @@ import math
 import os, sys, ogr
 from osgeo import gdal
 from gdalconst import *
+import rasterio
+from rasterio import crs
 
 
-
-def write_raster_inds(n,r,g,b,ds,sel,cols,rows):
+def write_raster_inds(n,r,g,b,ds,sel,rgb_File):
     print sel
     gam=1.7
-    bsize=min(cols,rows)/10
-    print bsize
 
-    geotransform = ds.GetGeoTransform()
-    originX = geotransform[0]
-    originY = geotransform[3] 
+
     mask = numpy.greater(n+r+g+b, 0)
     if sel==0:#DVI
         # print 0
         inds = numpy.choose(mask, (0,n-r))
     elif sel==1:##NDVI
         inds=numpy.choose(mask, (0,(n-r)/(n+r)))
-    elif sel==2:#GARI
-        inds=numpy.choose(mask, (0,(n-(g-gam*(b-r)))/(n+(g-gam*(b-r)))))
-    elif sel==3:#GNDVI
+#    elif sel==2:#GARI
+#        inds=numpy.choose(mask, (0,(n-(g-gam*(b-r)))/(n+(g-gam*(b-r)))))
+    elif sel==2:#GNDVI
         inds=numpy.choose(mask, (0,(n-g)/(n+g)))
         
-    elif sel==4:#OSAVI
+    elif sel==3:#OSAVI
         inds=numpy.choose(mask, (0,1.5*(n-r)/(n+r+0.16)))
-    elif sel==5:#RDVI
+    elif sel==4:#RDVI
         inds=numpy.choose(mask, (0,(n - r)/numpy.sqrt(n + r)))
-    elif sel==6: #RVI
-        inds= numpy.choose(mask, (0,n/r))
-    elif sel==7:#SAVI
+#    elif sel==6: #RVI
+#        inds= numpy.choose(mask, (0,n/r))
+    elif sel==5:#SAVI
         inds=numpy.choose(mask,(0,1.5*(n-r)/(n+r+0.5)) )
-    elif sel==8:#TDVI
+    elif sel==6:#TDVI
         inds=numpy.choose(mask, (0,numpy.sqrt(0.5+(n-r)/(n+r))))
     elif sel==9:
         inds=numpy.choose(mask, (0, (n-g)/(n + g)))
@@ -52,19 +49,45 @@ def write_raster_inds(n,r,g,b,ds,sel,cols,rows):
         inds=numpy.choose(mask, (0,200*(g*100)/(g*100+r*100+b*100)))
 
 #ave_i=(max_i-ave_i)/50+ave_i
-    maxi=numpy.max(inds)
-    mini=numpy.min(inds)
-    inds=inds*2/(maxi-mini)
-    inds=inds-1
-    mask2=numpy.equal(r+g+b,0)
-    # print numpy.size(mask2)
+#    print "bbeeeffoorreeee"
+#    print numpy.min(inds)
+#    print numpy.max(inds)
+#    print numpy.mean(inds)
+#    print numpy.var(inds)
+#    print "nnnooorrrmmmaaallliizzeeee"
+#
+    inds=inds-numpy.mean(inds)
+    inds=inds/numpy.std((inds))
     
+#    inds=inds/numpy.max((inds))
+#
+#    print numpy.min(inds)
+#    print numpy.max(inds)
+#    print numpy.mean(inds)
+#    jjjjj
+####normalize al together afterwards
+
+
+#    maxi=numpy.max(inds)
+#    mini=numpy.min(inds)
+#    inds=inds*2/(maxi-mini)
+#    inds=inds-1
+    mask2=numpy.equal(r+g+b,0)
+    inds+=numpy.ones((inds.shape[0],inds.shape[1]))*0.0001
+    # print numpy.size(mask2)
+# ccrs=crs.from_string("EPSG:32647")
     inds[mask2]=numpy.inf
-    driver = ds.GetDriver()
-    outDataset = driver.Create('Ind.tif', cols, rows, 1, GDT_Float32)
-    outBand = outDataset.GetRasterBand(1)
-    outBand.WriteArray(inds,0,0)
-     
+    src = rasterio.open(rgb_File)
+
+  
+    new_dataset =rasterio.open('Ind'+str(sel)+'.tif','w',driver='Gtiff',height=inds.shape[0], width=inds.shape[1],count=1, dtype=str(inds.dtype),crs=src.crs,transform=src.transform)
+    
+    new_dataset.write(inds, 1)
+    new_dataset .close()
+
+
+    
+    
 #     
 #    
 #    
--- get_Training_ind_Mats.py
+++ get_Training_ind_Mats.py
@@ -8,18 +8,20 @@ from calc_Inds_anth import *
 from qgis.core import *
 import libtiff
 import cv2
+import scipy.io as sio
+import rasterio
 
 
 
 def get_ind_image(sel_num,mask_path,rgb_FILE,nir_FILE,label):
     mask=QgsVectorLayer(mask_path,'mask','ogr')
-    expr = QgsExpression( 'id='+str(sel_num) )
+    expr = QgsExpression( 'Classvalue='+str(sel_num) )
     it = mask.getFeatures( QgsFeatureRequest( expr ) )
     ids = [i.id() for i in it]
     print ids
-                 
-                 
-                 
+    crs = mask.crs() 
+#                 
+#                 
     mask.setSelectedFeatures( ids )
     res = QgsVectorFileWriter.writeAsVectorFormat( mask,
         'mask_sel.shp',
@@ -29,92 +31,58 @@ def get_ind_image(sel_num,mask_path,rgb_FILE,nir_FILE,label):
          True #onlySelected
          )
 
+
+
+
 #QgsMapLayerRegistry.instance().addMapLayer(mask_sel)
 ####burn mask_sel_to another raster
-    out=os.system('gdalwarp -cutline mask_sel.shp  -crop_to_cutline ' +rgb_FILE+' rgb_cut.tif')
-####read cut tiff file 
-##RGB_cut= QgsRasterLayer(Proj_Path+'cut.tif','RGB_cut')
-##QgsMapLayerRegistry.instance().addMapLayer(RGB_cut)
-
-    out=os.system('gdalwarp -cutline mask_sel.shp  -crop_to_cutline ' +nir_FILE+' NIR_cut.tif')
-    NIR_cut= QgsRasterLayer('NIR_cut.tif','NIR_cut')
-    QgsMapLayerRegistry.instance().addMapLayer(NIR_cut)
     
-
-    ds = gdal.Open('rgb_cut.tif',GA_ReadOnly)
-    NIR_ds = gdal.Open('NIR_cut.tif',GA_ReadOnly)
-
+    ds = gdal.Open(rgb_FILE,GA_ReadOnly)
     cols = ds.RasterXSize
     rows = ds.RasterYSize
-    bands = ds.RasterCount
-
-    geotransform = ds.GetGeoTransform()
-    originX = geotransform[0]
-    originY = geotransform[3]
-    pixelWidth = geotransform[1]
-    pixelHeight = geotransform[5]
-
-
-
-    NIR_geotransform = NIR_ds.GetGeoTransform()
-    nband=NIR_ds.GetRasterBand(1)
-    
-
-    
-    ndata = nband.ReadAsArray(0, 0,cols, rows).astype(np.float32)
-    
-
-    ndata=ndata/(ndata.max())
-    xOffset =10# int((cols) / pixelWidth)
-    yOffset = 10#int((rows) / pixelHeight)
-
     rband=ds.GetRasterBand(1)
     gband=ds.GetRasterBand(2)
     bband=ds.GetRasterBand(3)
-
-
-
-
-    rdata = rband.ReadAsArray(0, 0,cols, rows).astype(np.float32)
-    gdata=gband.ReadAsArray(0, 0,cols, rows).astype(np.float32)
-    bdata=bband.ReadAsArray(0, 0,cols, rows).astype(np.float32)
+    nband=ds.GetRasterBand(4)
+    
+    
+    rdata = rband.ReadAsArray(0, 0,cols, rows).astype(numpy.float32)
+    gdata=gband.ReadAsArray(0, 0,cols, rows).astype(numpy.float32)
+    bdata=bband.ReadAsArray(0, 0,cols, rows).astype(numpy.float32)
+    ndata = nband.ReadAsArray(0, 0,cols, rows).astype(numpy.float32)
+    
 
     rdata=rdata/(rdata.max())
     gdata=gdata/(gdata.max())
     bdata=bdata/(bdata.max())
+    ndata=ndata/(ndata.max())
     
-    mask = np.equal(rdata+gdata+bdata, 0)
     X_train=[]
     Y_train=[]
     begin=1
-    for i in range (0,9):#####for over indices
-        write_raster_inds(ndata,rdata,gdata,bdata,ds,i,cols,rows)#8,
-        # ind_image= QgsRasterLayer('ind.tif', 'ind_image')
-    
-    
-
-# IOError: cannot identify image file
-
-        tif = libtiff.TIFF.open('ind.tif')
+    for i in range (1,6):#####for over indices
+        write_raster_inds(ndata,rdata,gdata,bdata,0,i,rgb_FILE)#8,
+        out=os.system('gdalwarp -cutline mask_sel.shp  -crop_to_cutline '+'ind'+str(i)+'.tif rgb_cut.tif')
+        tif = libtiff.TIFF.open('rgb_cut.tif')
         im = tif.read_image()
+        mask2=np.equal(im,0)
+        im[mask2]=np.inf
+        
         temp_i=np.ravel(im)
         temp_i=temp_i[~numpy.isinf(temp_i)]
+        temp_i=temp_i[~numpy.isnan(temp_i)]
         inds=np.where(temp_i!=np.nan)
-        
-        
 
-
-
-        
-#  im=im*255.0
-# cv2.imwrite('ind_'+str(i)+'_'+str(sel_num)+'.jpg',im)
         if begin==1:
             X_train=temp_i.T
             begin=0
         else:
             X_train=np.vstack((X_train,temp_i.T))
-
+            
+        sio.savemat('train_data.mat',{'X_train':X_train})
+        
+        out=os.system('rm '+'rgb_cut.tif')
+        hhh
     Y_train=label*np.ones(np.size(temp_i))
-    out=os.system('rm '+'NIR_cut.tif')
-    out=os.system('rm '+'rgb_cut.tif')
+    
     return (X_train.T,Y_train)
